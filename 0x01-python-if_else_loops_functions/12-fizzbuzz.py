66c2fd21138a8b72230998d7b4a40c13f8c3248e
Eyosi <116eyosi@gmail.com>
2021-09-07 22:23:00 +0000

    update

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,7 +3,6 @@ def fizzbuzz():
     for i in range(101):
         if ((i % 3) == 0 and (i % 5) == 0):
             print("FizzBuzz ", end='')
-            continue
         elif (i % 3) == 0:
             print("Fizz ", end='')
         elif (i % 5) == 0:
