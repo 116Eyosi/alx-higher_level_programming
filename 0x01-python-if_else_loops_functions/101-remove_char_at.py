ca1ae30fc97494d98ecd52e3e8a4dd01365775c6
Eyosi <116eyosi@gmail.com>
2021-09-07 22:33:35 +0000

    update

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+def remove_char_at(str, n):
+    str2 = []
+    if(n >= 0):
+        for i in range(len(str)):
+            if(i != n):
+                str2.append(str[i])
+        str1 = ''.join(str2)
+        return(str1)
+    return(str)
