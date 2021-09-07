8f2b69be309bd5c1d1f532c88ba8df74beb85b38
Eyosi <116eyosi@gmail.com>
2021-09-07 23:07:32 +0000

    update

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('a') - 1, -1):
-    print('{}'.format(chr(i - 32) if (i % 2 != 0) else chr(i)), end='')
+for c in range(122, 96, -1):
+    print("{:s}".format(chr(c) if (c % 2 == 0) else chr(c - 32)), end='')
