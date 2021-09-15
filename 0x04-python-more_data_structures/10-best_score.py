99b99d5f3b33c37a93a1e72939a06bc42dc759bc
Eyosi <116eyosi@gmail.com>
2021-09-15 14:17:50 +0000

    update

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is not None and len(a_dictionary) is not 0:
-        valist = list(sorted(a_dictionary.values()))
-        num = valist[0]
-        for k, v in sorted(a_dictionary.items()):
-            if v >= num:
-                num = v
-                max_score = k
-        return max_score
-    else:
+    if not a_dictionary:
         return None
+
+    maximun = -99999
+    key = ""
+
+    for k, v in a_dictionary.items():
+        if v >= maximun:
+            maximun = v
+            key = k
+
+    return key
