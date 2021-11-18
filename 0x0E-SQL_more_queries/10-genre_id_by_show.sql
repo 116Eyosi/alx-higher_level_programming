2e6f16f05d6d0432cdfad67333c9d12382658ac3
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-11-18 01:30:20 +0000

    q7

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,3 @@
+-- Lists all shows that have genre linked.
+SELECT s.`title`, g.`genre_id` FROM `tv_shows` AS s
+INNER JOIN `tv_show_genres` AS g ON s.`id` = g.`show_id` ORDER BY s.`title`, g.`genre_id`;
