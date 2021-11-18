fdf86801d0d6ec8d3ba48a3225c5db44e49bab47
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-11-18 00:46:34 +0000

    q3/4

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,2 +1,2 @@
 -- create Table name cant be null
-CREATE TABLE IF NOT EXISTS force_name(Dd INT, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS force_name(id INT, name VARCHAR(256) NOT NULL);
