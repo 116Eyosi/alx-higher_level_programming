c47ce78984f0ac32e3c3233322428c4eba9226f8
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-11-18 00:40:28 +0000

    q3/4

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,2 @@
+-- create Table name cant be null
+CREATE TABLE IF NOT EXISTS force_name(Dd INT, name VARCHAR(256) NOT NULL);
---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,2 @@
+-- Table where id has value
+CREATE TABLE IF NOT EXISTS id_not_Null(id INT NOT NULL DEFAULT 1,name VARCHAR(256));
